4dc5d7a310f0d10ca9f9566b4eea4c8e3efa1f60
Hridya Agrawal <hridayahoney@gmail.com>
2024-12-08 16:31:57 +0000

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,22 +6,20 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<Metadata> {
-  return {
-    title: `Redirect for ${params.slug}`,
-    description: 'URL Redirection Page',
-  };
-}
+type Params = {
+  slug: string;
+};
 
-export default async function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
+type PageProps = {
+  params: Params & {
+    then?: never;
+    catch?: never;
+    finally?: never;
+    [Symbol.toStringTag]?: never;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   const { slug } = params;
 
   const { data, error } = await supabase
@@ -35,6 +33,15 @@ export default async function Page({
   }
 
   return <div>URL not found</div>;
+}
+
+export async function generateMetadata({ 
+  params 
+}: PageProps): Promise<Metadata> {
+  return {
+    title: `Redirect for ${params.slug}`,
+    description: 'URL Redirection Page'
+  };
 }
 
 export const dynamic = 'force-dynamic';
